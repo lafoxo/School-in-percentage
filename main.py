98b35f0146579579629c26a997349a4b0d6e5a6d
lafoxo <info4lolo6@gmail.com>
2024-12-19 21:02:25 +0100

    commit

---
+++ main.py
@@ -0,0 +1,10 @@
+print("enter Marks Obtained in 4 subjects:")
+math=int(input("maths:"))
+English=int(input("English:"))
+science=int(input("science:"))
+technology=int(input("technology:"))
+sum=math+English+science+technology
+print("sum of math,English,science(biology and pysics)+technology")
+perc= (sum/400)*100
+print("end=percentage Mark=")
+print(perc)
